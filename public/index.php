d462bc2b28be8fa3a79135f0eb09d0d42d3c50a7
gitgaojun <1148743058@qq.com>
2017-07-22 02:09:36 +0800

    delete message

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
+//echo 4;exit;
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
